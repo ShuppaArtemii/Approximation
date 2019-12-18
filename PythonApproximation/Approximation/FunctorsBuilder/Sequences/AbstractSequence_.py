23625e58bc9bcd5778870a0511d7151414f426e3
Артемий Шуппа <artem.shuppa@gmail.com>
2019-12-18 12:17:21 +0500

    remove old project

--- PythonApproximation/Approximation/FunctorsBuilder/Sequences/AbstractSequence_.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class AbstractSequence_:
-    @abstractmethod
-    def GetSequence(modifiedFunctors, start, stop, step = 1):
-        sequence = [];
-        for i in range(start, stop, step):
-            sequence.append(modifiedFunctors[i]);
-        return sequence;
